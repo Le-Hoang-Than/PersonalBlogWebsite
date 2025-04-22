73f0e6368c58447f771cfb7d4eddbda122345018
Le Hoang Than <lehoangthan584@gmail.com>
2025-04-22 21:31:28 +0700

    fix: base tag

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,19 +1,19 @@
 const isLocal =
   location.hostname === "127.0.0.1" || location.hostname === "localhost";
 const base = document.createElement("base");
-base.href = isLocal ? "" : "/PersonalBlogWebsite";
+base.href = isLocal ? "." : "/PersonalBlogWebsite";
 document.head.appendChild(base);
-const basePath =
-  location.hostname === "127.0.0.1" ? "" : "/PersonalBlogWebsite";
-// Load navbar
-$("#Navbar").load(`${basePath}/blog/components/navbar.html`);
+// const basePath =
+//   location.hostname === "127.0.0.1" ? "" : "/PersonalBlogWebsite";
+// // Load navbar
+// $("#Navbar").load(`${basePath}/blog/components/navbar.html`);
 
-// Load ruler
-$("#toggle-btn").load(`${basePath}/blog/components/ruler.html`, function () {
-  let isBorderOn = false;
+// // Load ruler
+// $("#toggle-btn").load(`${basePath}/blog/components/ruler.html`, function () {
+//   let isBorderOn = false;
 
-  $("#toggle-btn").on("click", function () {
-    isBorderOn = !isBorderOn;
-    $("body").toggleClass("border", isBorderOn);
-  });
-});
+//   $("#toggle-btn").on("click", function () {
+//     isBorderOn = !isBorderOn;
+//     $("body").toggleClass("border", isBorderOn);
+//   });
+// });
