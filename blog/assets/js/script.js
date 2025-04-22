7cd376877be2f5521d52e56506b72d4f6ea3d44d
Le Hoang Than <lehoangthan584@gmail.com>
2025-04-22 21:12:44 +0700

    fix: base tag

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,13 +1,15 @@
-  const isLocal = location.hostname === "127.0.0.1" || location.hostname === "localhost";
-  const base = document.createElement("base");
-  base.href = isLocal ? "" : "/PersonalBlogWebsite/";
-  document.head.appendChild(base);
-const basePath = location.hostname === "127.0.0.1" ? "" : "/PersonalBlogWebsite";
+const isLocal =
+  location.hostname === "127.0.0.1" || location.hostname === "localhost";
+const base = document.createElement("base");
+base.href = isLocal ? "" : "/PersonalBlogWebsite";
+document.head.appendChild(base);
+// const basePath =
+//   location.hostname === "127.0.0.1" ? "" : "/PersonalBlogWebsite";
 // Load navbar
-$("#Navbar").load(`${basePath}/blog/components/navbar.html`);
+$("#Navbar").load("/blog/components/navbar.html");
 
 // Load ruler
-$("#toggle-btn").load(`${basePath}/blog/components/ruler.html`, function () {
+$("#toggle-btn").load("/blog/components/ruler.html", function () {
   let isBorderOn = false;
 
   $("#toggle-btn").on("click", function () {
