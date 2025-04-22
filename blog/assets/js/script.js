c4a1a1156679b15c1d95819bd4b94a630bb01276
Le Hoang Than <lehoangthan584@gmail.com>
2025-04-22 20:25:19 +0700

    fix: base tag

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,3 +1,7 @@
+  const isLocal = location.hostname === "127.0.0.1" || location.hostname === "localhost";
+  const base = document.createElement("base");
+  base.href = isLocal ? "/" : "/PersonalBlogWebsite/";
+  document.head.appendChild(base);
 const basePath = location.hostname === "127.0.0.1" ? "" : "/PersonalBlogWebsite";
 // Load navbar
 $("#Navbar").load(`${basePath}/components/navbar.html`);
