15ddcf7dd155965b8f92063c39f046be7a7a056b
Le Hoang Than <lehoangthan584@gmail.com>
2025-04-22 09:59:06 +0700

    add: jquery

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,37 +1,12 @@
-async function loadruler() {
-  try {
-    const response = await fetch("/blog/components/ruler.html");
-    const rulerHtml = await response.text();
-    document.querySelector("#toggle-btn").innerHTML = rulerHtml;
-  } catch (error) {
-    console.error("Lỗi khi tải navbar:", error);
-    document.querySelector("#toggle-btn").innerHTML =
-      "<p>Không thể tải ruler.</p>";
-  }
-}
-loadruler();
-const toggleBtn = document.getElementById("toggle-btn");
-let isBorderOn = false;
+// Load navbar
+$("#Navbar").load("/blog/components/navbar.html");
 
-toggleBtn.addEventListener("click", function () {
+// Load ruler
+$("#toggle-btn").load("/blog/components/ruler.html", function () {
+  let isBorderOn = false;
+
+  $("#toggle-btn").on("click", function () {
     isBorderOn = !isBorderOn;
-
-    if (isBorderOn) {
-        document.body.classList.add("border");
-    } else {
-        document.body.classList.remove("border");
-    }
+    $("body").toggleClass("border", isBorderOn);
+  });
 });
-
-async function loadNavbar() {
-  try {
-    const response = await fetch("/blog/components/navbar.html");
-    const navbarHtml = await response.text();
-    document.querySelector("#Navbar").innerHTML = navbarHtml;
-  } catch (error) {
-    console.error("Lỗi khi tải navbar:", error);
-    document.querySelector("#Navbar").innerHTML =
-      "<p>Không thể tải navbar.</p>";
-  }
-}
-loadNavbar(); // Gọi hàm để tải navbar khi trang tải xong
